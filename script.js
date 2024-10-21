ae4f0446d450781be503d813d41774a27ce05033
Dileep Rajoriya <139870968+Er-Dileep-rajoriya@users.noreply.github.com>
2024-10-21 23:10:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+  //Write your code here
+
+  let sampleTable = document.getElementById("sampleTable");
+
+  let newRow = sampleTable.insertRow(0);
+
+  let cell1 = newRow.insertCell(0);
+  let cell2 = newRow.insertCell(1);
+
+  cell1.innerText = "New Cell1";
+  cell2.innerText = "New Cell2";
 }
